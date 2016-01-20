3be23b63d02ed0c658ae526579f822a22aa9d21e
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2016-01-20 15:09:45 +0100

    added first working version of m1-graph with timed edges

--- src/dna/graph/generators/network/NetworkBatch.java
+++ src/dna/graph/generators/network/NetworkBatch.java
@@ -2,11 +2,13 @@ package dna.graph.generators.network;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.joda.time.DateTime;
 
 import dna.graph.Graph;
+import dna.graph.edges.Edge;
 import dna.graph.nodes.Node;
 import dna.updates.batch.Batch;
 import dna.updates.generators.BatchGenerator;
@@ -51,7 +53,9 @@ public abstract class NetworkBatch extends BatchGenerator {
 	}
 
 	public abstract void onEvent(Graph g, Batch b, NetworkEvent e,
-			HashMap<Integer, Node> portMap, HashMap<String, Node> ipMap);
+			HashMap<Integer, Node> portMap, HashMap<String, Node> ipMap,
+			HashMap<Integer, Integer> nodeDegreeChangeMap,
+			ArrayList<Edge> addedEdges, ArrayList<Edge> removedEdges);
 
 	@Override
 	public Batch generate(Graph graph) {
@@ -61,9 +65,13 @@ public abstract class NetworkBatch extends BatchGenerator {
 
 		HashMap<Integer, Node> portMap = new HashMap<Integer, Node>();
 		HashMap<String, Node> ipMap = new HashMap<String, Node>();
+		HashMap<Integer, Integer> nodeDegreeChangeMap = new HashMap<Integer, Integer>();
+		ArrayList<Edge> addedEdges = new ArrayList<Edge>();
+		ArrayList<Edge> removedEdges = new ArrayList<Edge>();
 
 		if (this.bufferedEntry != null)
-			onEvent(graph, b, this.bufferedEntry, portMap, ipMap);
+			onEvent(graph, b, this.bufferedEntry, portMap, ipMap,
+					nodeDegreeChangeMap, addedEdges, removedEdges);
 
 		while (this.reader.isNextEventPossible()) {
 			TCPEvent e = this.reader.getNextEvent();
@@ -78,10 +86,31 @@ public abstract class NetworkBatch extends BatchGenerator {
 			if (time.isAfter(this.threshold)) {
 				this.threshold = this.threshold.plusSeconds(this.batchLength);
 				this.bufferedEntry = e;
+
+				// print mappings (FOR DEBUG)
+				// reader.printMappings();
+
+				// if (reader.isRemoveZeroDegreeNodes()) {
+				// for (IElement n_ : graph.getNodes()) {
+				// Node n = (Node) n_;
+				//
+				// int change = 0;
+				// if (nodeDegreeChangeMap.containsKey(n.getIndex()))
+				// change = nodeDegreeChangeMap.get(n.getIndex());
+				// System.out.println("blablain node " + n
+				// + "  ?? \tdegree: " + n.getDegree()
+				// + "\tchange: " + change +"\tremove: " + ((n.getDegree() +
+				// change) <= 0));
+				// if ((n.getDegree() + change) <= 0)
+				// b.add(new NodeRemoval(n));
+				// }
+				// }
+
 				return b;
 			} else {
 				// handle changes
-				this.onEvent(graph, b, e, portMap, ipMap);
+				this.onEvent(graph, b, e, portMap, ipMap, nodeDegreeChangeMap,
+						addedEdges, removedEdges);
 			}
 		}
 
