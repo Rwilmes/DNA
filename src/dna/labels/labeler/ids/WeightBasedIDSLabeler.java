a2260b23773a92bc4b1c65045d915e4145c77b36
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2016-04-06 17:10:23 +0200

    added weightbased ids labeler

---
+++ src/dna/labels/labeler/ids/WeightBasedIDSLabeler.java
@@ -0,0 +1,69 @@
+package dna.labels.labeler.ids;
+
+import java.util.ArrayList;
+
+import dna.graph.Graph;
+import dna.graph.generators.GraphGenerator;
+import dna.labels.Label;
+import dna.labels.labeler.Labeler;
+import dna.metrics.IMetric;
+import dna.metrics.degree.WeightedDegreeDistributionR;
+import dna.series.data.BatchData;
+import dna.series.data.MetricData;
+import dna.updates.batch.Batch;
+import dna.updates.generators.BatchGenerator;
+import dna.util.Log;
+
+/**
+ * Second approach of an IDS-labeler based on model B.
+ * 
+ * @author Rwilmes
+ * 
+ */
+public class WeightBasedIDSLabeler extends Labeler {
+
+	private static String name = "ids-wbl";
+	private static String type = "M2";
+
+	private int threshold = 400;
+
+	public WeightBasedIDSLabeler() {
+		super("IDSWeightBasedLabeler");
+	}
+
+	public WeightBasedIDSLabeler(int threshold) {
+		this();
+		this.threshold = threshold;
+	}
+
+	@Override
+	public boolean isApplicable(GraphGenerator gg, BatchGenerator bg,
+			IMetric[] metrics) {
+		if (Labeler.getMetric(metrics, WeightedDegreeDistributionR.class) == null) {
+			Log.warn(getName() + ": metric '" + "WeightedDegreeDistribuutionR"
+					+ "' not found!");
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public ArrayList<Label> computeLabels(Graph g, Batch batch,
+			BatchData batchData, IMetric[] metrics) {
+		ArrayList<Label> list = new ArrayList<Label>();
+
+		MetricData weightedDegrees = batchData.getMetrics().get(
+				"WeightedDegreeDistributionR");
+
+		double max = weightedDegrees.getValues().get("WeightedInDegreeMax")
+				.getValue();
+
+		if (max >= threshold) {
+			Label l = new Label(name, type, "true");
+			list.add(l);
+			Log.info(batchData.getTimestamp() + "  <-  " + l.toString());
+		}
+
+		return list;
+	}
+}
