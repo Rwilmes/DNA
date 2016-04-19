1926fcf4c6e343725ab10c68a9f464dc572b0237
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2016-04-19 10:59:54 +0200

    added DatasetUtils class

---
+++ src/dna/graph/generators/network/tests/DatasetUtils.java
@@ -0,0 +1,28 @@
+package dna.graph.generators.network.tests;
+
+public class DatasetUtils {
+
+	/**
+	 * Returns the absolute date of a DARPA 1998 week and day pair in
+	 * dd-MM-yyyy.
+	 **/
+	public static String getDarpaDate(int week, int day) {
+		int d = 1;
+		int m = 6;
+
+		int days = (week - 1) * 7 + (day - 1);
+
+		if (days > 31) {
+			d += (days - 30);
+			m++;
+		} else {
+			d += days;
+		}
+
+		String ds = (d > 9) ? "" + d : "0" + d;
+		String ms = (m > 9) ? "" + m : "0" + m;
+		String ys = "1998";
+
+		return ds + "-" + ms + "-" + ys;
+	}
+}
