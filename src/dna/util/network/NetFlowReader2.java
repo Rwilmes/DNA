f6197de7627a8a8faec757c1b7dc679197e0474d
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2016-04-25 15:17:10 +0200

    changed netflow-fields in reader

--- src/dna/util/network/NetFlowReader2.java
+++ src/dna/util/network/NetFlowReader2.java
@@ -9,14 +9,16 @@ public class NetFlowReader2 extends TCPEventReader {
 
 	public NetFlowReader2(String dir, String filename)
 			throws FileNotFoundException {
-		super(dir, filename, "\t", "MM-dd-yyyy HH:mm:ss.SSSSSS", "ss.SSSSSS",
-				null, new TCPEventField[] { TCPEventField.TIME,
-						TCPEventField.DURATION, TCPEventField.NONE,
-						TCPEventField.SERVICE, TCPEventField.SRC_PORT,
-						TCPEventField.DST_PORT, TCPEventField.SRC_IP,
-						TCPEventField.DST_IP, TCPEventField.DST_IP,
+		super(dir, filename, "\t", "MM-dd-yyyy HH:mm:ss", "ss.SSSSSS", null,
+				new TCPEventField[] { TCPEventField.TIME, TCPEventField.NONE,
+						TCPEventField.NONE, TCPEventField.SERVICE,
+						TCPEventField.SRC_IP, TCPEventField.NONE,
+						TCPEventField.DST_IP, TCPEventField.NONE,
 						TCPEventField.NONE, TCPEventField.NONE,
-						TCPEventField.NONE, TCPEventField.NONE });
+						TCPEventField.NONE, TCPEventField.NONE,
+						TCPEventField.NONE, TCPEventField.NONE,
+						TCPEventField.NONE, TCPEventField.SRC_PORT,
+						TCPEventField.DST_PORT });
 	}
 
 }
