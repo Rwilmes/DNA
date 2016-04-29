4aac536e011af6fb3ca811a524a6137e2eb0bc6d
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2016-04-29 17:18:00 +0200

    added new version of data collector (missing labels)

--- src/dna/util/machineLearning/DataCollector.java
+++ src/dna/util/machineLearning/DataCollector.java
@@ -1,196 +1,301 @@
 package dna.util.machineLearning;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import argList.ArgList;
+import argList.types.array.IntArrayArg;
+import argList.types.array.StringArrayArg;
+import argList.types.atomic.BooleanArg;
+import argList.types.atomic.EnumArg;
+import argList.types.atomic.StringArg;
 import dna.io.Writer;
 import dna.io.filesystem.Dir;
 import dna.labels.Label;
-import dna.labels.LabelList;
 import dna.plot.PlotConfig;
 import dna.series.aggdata.AggregatedBatch.BatchReadMode;
 import dna.series.data.BatchData;
-import dna.series.data.RunData;
-import dna.series.data.SeriesData;
-import dna.series.lists.BatchDataList;
 import dna.util.Config;
 import dna.util.Log;
 
 public class DataCollector {
 
 	public static void main(String[] args) throws IOException {
-		if (args.length <= 6 || args[0].equals("-h") || args[0].equals("-help")) {
-			System.out
-					.println("not enough / wrong parameters? displaying help:");
-			System.out
-					.println("Input: [seriesData-dir], [runId], [batchZipMode], [out-filename], [-l label1:type1 label1:type2 label2:type1 ...] [-m metric1~v1 metric1~v2 metric2~v1 ...]*");
-			Log.infoSep();
-			System.out.println("seriesData-dir:\t"
-					+ "Dir of the series-data. Should contain run.X dir.");
-			System.out.println("runId\t\t"
-					+ "Id of the run to be used. Usually 0.");
-			System.out
-					.println("batchZipMode\t"
-							+ "Mode in which batches are saved. Valid values: none, batches, runs.");
-			System.out.println("out-filename\t"
-					+ "Name of the file the output will be written to.");
-			System.out
-					.println("-l label:type\t"
-							+ "Signals the beginning of a list of labels to be included in output data.");
-			System.out
-					.println("-m metric1~v1\t"
-							+ "Signals the beginning of a list of metric-value pairs to be added to the data.");
-			return;
-		}
+		ArgList<DataCollector> argList = new ArgList<DataCollector>(
+				DataCollector.class,
+				new StringArrayArg("seriesDirs", "list of source-series-dirs",
+						","),
+				new IntArrayArg("runIds", "ids of the respective runs", ","),
+				new EnumArg("zip-mode",
+						"zip-mode of the series. Possible values",
+						new String[] { "none, batches, runs" }),
+				new BooleanArg(
+						"includeUnavailable",
+						"if true, even batches that are not contained in all series and runs will be included"),
+				new StringArg("unavailable-char",
+						"character used for values that are not available."),
+				new EnumArg(
+						"timestamp-format",
+						"toggles if timestamp (dd-MM-yyyy HH:mm:ss) or DARPA98 week-day (w-d HH:mm:ss) will be used by the timestamp fields",
+						new String[] { "timestamp", "week_day" }),
+				new StringArg("from", "starting timestamp"),
+				new StringArg("to", "maximum timestamp"),
+				new StringArg("output", "path of the output file"),
+				new StringArrayArg(
+						"labels",
+						"list of labels to be included of format: <label>:<type>",
+						","),
+				new StringArrayArg(
+						"metrics",
+						"list of metrics to be included of format: <metric>~<value>. Use <seriesId>:<metric>~<value> to only include the metric-value pair of a specific series.",
+						","));
+		DataCollector d = argList.getInstance(args);
+		d.collect();
+	}
 
-		long start = System.currentTimeMillis();
+	protected String[] seriesDirs;
+	protected Integer[] runIds;
+	protected String zipMode;
+	protected String collectionMode;
+	protected boolean includeUnavailable;
+	protected String timestampFormat;
+	protected DateTimeFormatter fmt = DateTimeFormat
+			.forPattern("dd-MM-yyyy-HH:mm:ss");
+	protected DateTime from;
+	protected DateTime to;
+	protected String outputPath;
+	protected String notAvailableChar;
+	protected String[] labels;
+	protected String[] metrics;
 
-		String dir = args[0];
-		int runId = Integer.parseInt(args[1]);
-		String readMode = args[2];
-		switch (readMode) {
-		case "none":
-			Config.zipNone();
-			break;
+	public DataCollector(String[] seriesDirs, Integer[] runIds, String zipMode,
+			Boolean includeUnavailable, String notAvailableChar,
+			String timestampFormat, String from, String to, String outputPath,
+			String[] labels, String[] metrics) {
+		this.seriesDirs = seriesDirs;
+		this.runIds = runIds;
+		this.zipMode = zipMode;
+		this.includeUnavailable = includeUnavailable;
+		this.timestampFormat = timestampFormat;
+		this.outputPath = outputPath;
+		this.notAvailableChar = notAvailableChar;
+		this.labels = labels;
+		this.metrics = metrics;
+
+		// zip mode
+		switch (this.zipMode) {
 		case "batches":
 			Config.zipBatches();
 			break;
 		case "runs":
 			Config.zipRuns();
 			break;
-		}
-		String outDir = args[3];
-
-		int labelOffset = 4;
-		int metricOffset = 6;
-
-		for (int i = 0; i < args.length; i++) {
-			if (args[i].equals("-l"))
-				labelOffset = i + 1;
-			if (args[i].equals("-m"))
-				metricOffset = i + 1;
+		case "none":
+			Config.zipNone();
+			break;
 		}
 
-		String[] inputLabels = new String[metricOffset - labelOffset - 1];
-		for (int i = 0; i < inputLabels.length; i++) {
-			inputLabels[i] = args[i + labelOffset];
+		// timestamps
+		if (timestampFormat.equals("timestamp")) {
+			this.from = this.fmt.parseDateTime(from);
+			this.to = this.fmt.parseDateTime(to);
+		}
+		if (timestampFormat.equals("week_day")) {
+			String dateFrom = getDarpaDate(
+					Integer.parseInt("" + from.charAt(0)),
+					Integer.parseInt("" + from.charAt(2)));
+			this.from = this.fmt.parseDateTime(dateFrom + "-"
+					+ from.substring(4));
+			String dateTo = getDarpaDate(Integer.parseInt("" + to.charAt(0)),
+					Integer.parseInt("" + to.charAt(2)));
+			this.to = this.fmt.parseDateTime(dateTo + "-" + to.substring(4));
+		}
+	}
+
+	/**
+	 * Collects the data and writes the results
+	 * 
+	 * @throws IOException
+	 **/
+	public void collect() throws IOException {
+		// start time
+		long start = System.currentTimeMillis();
+
+		Log.infoSep();
+
+		String[] runDirs = new String[seriesDirs.length];
+		for (int i = 0; i < seriesDirs.length; i++) {
+			runDirs[i] = Dir.getRunDataDir(seriesDirs[i], runIds[i]);
 		}
 
-		String[] inputMetrics = new String[args.length - metricOffset];
-		for (int i = 0; i < inputMetrics.length; i++) {
-			inputMetrics[i] = args[i + metricOffset];
+		String[] batches = getBatches(runDirs, from, to, includeUnavailable);
+
+		Log.info(batches.length + " batches:");
+		for (int i = 0; i < batches.length; i++) {
+			if (i > 2) {
+				Log.info("\t...");
+				i = batches.length - 1;
+			}
+			Log.info("\t"
+					+ batches[i]
+					+ "\t\t"
+					+ new DateTime(Dir.getTimestamp(batches[i]) * 1000)
+							.toString());
+		}
+		Log.infoSep();
+
+		// gather metrics
+		@SuppressWarnings("unchecked")
+		ArrayList<String>[] metricsList = new ArrayList[seriesDirs.length];
+		for (int i = 0; i < metricsList.length; i++) {
+			metricsList[i] = new ArrayList<String>();
 		}
 
-		System.out.println("labels:");
-		for (String s : inputLabels)
-			System.out.println("\t" + s);
-
-		System.out.println("metrics:");
-		for (String s : inputMetrics)
-			System.out.println("\t" + s);
-
-		String[] domains = new String[inputMetrics.length];
-		String[] values = new String[inputMetrics.length];
-
-		for (int i = 0; i < inputMetrics.length; i++) {
-			String[] split = inputMetrics[i].split("~");
-			domains[i] = split[0];
-			values[i] = split[1];
+		for (String m : this.metrics) {
+			// check if only for one series
+			if (m.contains(":")) {
+				String[] splits = m.split(":");
+				int index = Integer.parseInt(splits[0]);
+				if (!metricsList[index].contains(splits[1]))
+					metricsList[index].add(splits[1]);
+			} else {
+				// add for all series
+				for (int i = 0; i < seriesDirs.length; i++) {
+					if (!metricsList[i].contains(m))
+						metricsList[i].add(m);
+				}
+			}
 		}
 
-		System.out.println("read from dir:");
-		System.out.println("\t" + dir);
+		String header = "Labels";
 
-		// String dir = "data/darpa1998_small/w1/monday/1_3600/";
-		int run = 0;
-		SeriesData sd = SeriesData.read(dir, "1_3600", false, false);
-
-		ArrayList<RunData> rdl = sd.getRuns();
-
-		BatchDataList bdl = sd.getRun(run).getBatches();
-
-		Writer w = new Writer("", outDir);
-		String header = "";
-		for (int i = 0; i < inputLabels.length; i++) {
-			if (i == 0)
-				header += inputLabels[i];
-			else
-				header += "\t" + inputLabels[i];
+		for (int i = 0; i < metricsList.length; i++) {
+			System.out.println("s" + i);
+			for (String s : metricsList[i]) {
+				System.out.println("\t" + s);
+				header += "\t" + "s" + i + ":" + s;
+			}
 		}
-		for (int i = 0; i < inputMetrics.length; i++) {
-			if (i == 0 && header.length() == 0)
-				header += inputMetrics[i];
-			else
-				header += "\t" + inputMetrics[i];
-		}
+
+		System.out.println(header);
+
+		Writer w = new Writer("", outputPath);
 		w.writeln(header);
 
-		System.out.println("gathering data from batches..");
-		int amountBatches = bdl.size();
-		int counter = 0;
-		for (BatchData b : bdl.getList()) {
-			long timestamp = b.getTimestamp();
+		// iterate over batches
+		for (int i = 0; i < batches.length; i++) {
+			long timestamp = Dir.getTimestamp(batches[i]);
 
-			BatchData bd = BatchData.readIntelligent(
-					Dir.getBatchDataDir(dir, run, timestamp), timestamp,
-					BatchReadMode.readOnlySingleValues);
+			String line = "blub";
 
-			String buff = "";
-			boolean first = (buff.length() == 0);
-			for (int i = 0; i < inputLabels.length; i++) {
-				Label l = parseLabel(inputLabels[i]);
-				String v = "0.0";
+			// iterate over series
+			for (int j = 0; j < seriesDirs.length; j++) {
+				BatchData batchData = null;
+				System.out.println(i + "\t" + j);
 
-				LabelList ll = bd.getLabels();
-				for (Label label : ll.getList()) {
-					if (label.equals(l)) {
-						v = "1.0";
+				try {
+					batchData = BatchData.readIntelligent(Dir.getBatchDataDir(
+							seriesDirs[j], runIds[j], timestamp), timestamp,
+							BatchReadMode.readAllValues);
+				} catch (FileNotFoundException e) {
+				}
+
+				ArrayList<String> metrics = metricsList[j];
+				for (int k = 0; k < metrics.size(); k++) {
+					String metric = metrics.get(k);
+					String v;
+
+					String[] splits = metric.split("~");
+					String domain = splits[0];
+					String value = splits[1];
+
+					System.out.println("dom:\t" + domain);
+					System.out.println("val:\t" + value);
+
+					if (batchData == null || !batchData.contains(domain, value)) {
+						v = this.notAvailableChar;
+					} else {
+						v = getValue(batchData, domain, value);
 					}
+
+					line += "\t" + v;
 				}
 
-				if (!first)
-					buff += "\t" + v;
-				else {
-					buff += v;
-					first = false;
-				}
 			}
 
-			first = (buff.length() == 0);
-			for (int i = 0; i < domains.length; i++) {
-				String domain = domains[i];
-				String value = values[i];
-				String v;
-
-				if (bd.contains(domain, value))
-					v = getValue(bd, domain, value);
-				else
-					v = "-";
-
-				if (!first)
-					buff += "\t" + v;
-				else {
-					buff += v;
-					first = false;
-				}
-			}
-
-			w.writeln(buff);
-
-			counter++;
-			// System.out.println(counter);
-
-			if (counter % 100 == 0 || counter == amountBatches) {
-				System.out.println((int) Math.floor(1.0 * counter
-						/ amountBatches * 100)
-						+ "%" + "\t" + counter + " of " + amountBatches);
-			}
+			w.writeln(line);
 		}
 
 		w.close();
-		System.out.println("done in "
+		Log.info("done in "
 				+ ((1.0 * System.currentTimeMillis() - start) / 1000)
 				+ " seconds.");
+	}
+
+	/** Returns a list of available batches in respect to the parameters. **/
+	public String[] getBatches(String[] dirs, DateTime fromDateTime,
+			DateTime toDateTime, boolean includeUnavailable) throws IOException {
+		long from = 0;
+		long to = Long.MAX_VALUE;
+		if (fromDateTime != null) {
+			from = (long) Math.floor(fromDateTime.getMillis() / 1000);
+			Log.info("from: " + "\t" + from + "\t" + fromDateTime.toString());
+		}
+		if (toDateTime != null) {
+			to = (long) Math.floor(toDateTime.getMillis() / 1000);
+			Log.info("to: " + "\t\t" + to + "\t" + toDateTime.toString());
+		}
+
+		ArrayList<String> batchesList = new ArrayList<String>();
+		ArrayList<Integer> batchesValue = new ArrayList<Integer>();
+
+		int maxValue = 0;
+
+		for (int i = 0; i < dirs.length; i++) {
+			int value = 2 ^ (i);
+			maxValue += value;
+
+			String[] batches = Dir.getBatchesIntelligent(dirs[i]);
+
+			for (int j = 0; j < batches.length; j++) {
+				String batch = batches[j];
+				long timestamp = Dir.getTimestamp(batch);
+				if (timestamp < from || to < timestamp)
+					continue;
+				if (!batchesList.contains(batch)) {
+					batchesList.add(batch);
+					batchesValue.add(value);
+				} else {
+					int index = batchesList.indexOf(batch);
+					batchesValue.set(index, batchesValue.get(index) + value);
+				}
+			}
+		}
+
+		Log.infoSep();
+
+		ArrayList<String> selectedBatches = new ArrayList<String>();
+
+		if (includeUnavailable) {
+			selectedBatches = batchesList;
+		} else {
+			for (int i = 0; i < batchesList.size(); i++) {
+				if (batchesValue.get(i) == maxValue)
+					selectedBatches.add(batchesList.get(i));
+			}
+		}
+
+		batchesList = null;
+		batchesValue = null;
+
+		Collections.sort(selectedBatches);
+		return selectedBatches.toArray(new String[selectedBatches.size()]);
 	}
 
 	public static Label parseLabel(String line) {
@@ -216,5 +321,29 @@ public class DataCollector {
 					+ b.getMetrics().get(domain).getValues().get(value)
 							.getValue();
 		}
+	}
+
+	/**
+	 * Returns the absolute date of a DARPA 1998 week and day pair in
+	 * dd-MM-yyyy.
+	 **/
+	public static String getDarpaDate(int week, int day) {
+		int d = 1;
+		int m = 6;
+
+		int days = (week - 1) * 7 + (day - 1);
+
+		if (days > 31) {
+			d += (days - 30);
+			m++;
+		} else {
+			d += days;
+		}
+
+		String ds = (d > 9) ? "" + d : "0" + d;
+		String ms = (m > 9) ? "" + m : "0" + m;
+		String ys = "1998";
+
+		return ds + "-" + ms + "-" + ys;
 	}
 }
